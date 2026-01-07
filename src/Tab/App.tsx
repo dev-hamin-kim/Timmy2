403ced954d9c48cfa328fa63d4cab97eb0ac57ab
dev-hamin-kim <129831340+dev-hamin-kim@users.noreply.github.com>
2026-01-07 17:50:03 +0900

    chore: cleaned up App.tsx

--- src/Tab/App.tsx
+++ src/Tab/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-import * as teamJS from "@microsoft/teams-js";
+import { app, LiveShareHost } from "@microsoft/teams-js";
 import { LiveShareProvider } from "@microsoft/live-share-react";
 import {
   FluentProvider,
@@ -11,59 +11,30 @@ import {
 import { LiveCanvasPage } from "./LiveCanvasPage";
 import "./App.css";
 
-enum TeamsTheme {
-  HighContrast = "teamsHighContrastTheme",
-  Dark = "teamsDarkTheme",
-  Default = "teamsTheme",
-}
 
 export default function App() {
-  const [host, setHost] = useState<teamJS.LiveShareHost | undefined>();
-  const [appAppearance, setAppAppearance] = useState<TeamsTheme>(
-    TeamsTheme.Default
-  );
+  const [host, setHost] = useState<LiveShareHost | undefined>();
 
   useEffect(() => {
-    teamJS.app.initialize().then(async () => {
-      setHost(teamJS.LiveShareHost.create());
-    });
-
-    teamJS.app.getContext().then((context: teamJS.app.Context) => {
-      setAppAppearance(initTeamsTheme(context.app.theme));
-      teamJS.app.notifySuccess();
-    });
-
-    teamJS.app.registerOnThemeChangeHandler((theme) => {
-      setAppAppearance(initTeamsTheme(theme));
+    app.initialize().then(async () => {
+      setHost(LiveShareHost.create());
+      app.notifySuccess();
     });
   }, []);
 
-  if (host) {
-    return (
-      <FluentProvider theme={teamsLightTheme}>
+  return (
+    <FluentProvider theme={teamsLightTheme}>
+      {host ? (
         <LiveShareProvider host={host} joinOnLoad>
           <div className="App">
             <LiveCanvasPage />
           </div>
         </LiveShareProvider>
-      </FluentProvider>
-    );
-  } else {
-    return (
-      <FluentProvider theme={teamsLightTheme}>
+      ) : (
         <Spinner />
-      </FluentProvider>
-    );
-  }
+      )}
+    </FluentProvider>
+  );
 }
 
-function initTeamsTheme(theme: string | undefined): TeamsTheme {
-  switch (theme) {
-    case "dark":
-      return TeamsTheme.Dark;
-    case "contrast":
-      return TeamsTheme.HighContrast;
-    default:
-      return TeamsTheme.Default;
-  }
-}
+
